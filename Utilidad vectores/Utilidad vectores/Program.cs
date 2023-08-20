cdca5002e760c15433eaaa0416a0eb9f9eb19dc5
HugoPazR <hrpaz@unicesar.edu.co>
2023-08-19 22:56:21 -0500

    Agrege la carpeta add.

---
+++ Utilidad vectores/Utilidad vectores/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilidad_vectores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aplicacion de utilidad de los vectores");
+            Console.WriteLine("\nPulse una tecla para salir..."); Console.ReadKey();
+        }
+    }
+}
